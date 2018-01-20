a4f05e7332e634ede7b248e597d4446829a8a09d
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-01-20 16:24:56 -0500

    function to be implemented

---
+++ scripts/label_parser.py
@@ -0,0 +1,3 @@
+'''Takes in a list of dictionaries corresponding to outputs of model,
+returns array of tuples corresponding to topleft and bottomright corners of
+bounding box for objects labeled as 'person'.'''
