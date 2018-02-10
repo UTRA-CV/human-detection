e8eea610b1c642d03039673fc3b6f7b399f7233b
Stew Esho <StewardEsho@gmail.com>
2018-02-10 15:34:06 -0500

    Tweaked darkflow options

--- find_humans.py
+++ find_humans.py
@@ -4,7 +4,7 @@ import glob
 import time
 
 if __name__ == '__main__':
-    options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.1}
+    options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.25, "gpu": 1.0}
 
     tfnet = TFNet(options)
 
@@ -12,19 +12,20 @@ if __name__ == '__main__':
     images = [cv2.imread(img) for img in filenames]
 
 
+
     print('Filtering the persons . . . ')
     start_time = time.time()
+
     for i in range(len(images)):
         results = tfnet.return_predict(images[i])  # results is in JSON format
-        # filtered_results = []
-        filtered_results = [item for item in results if
-                            (item['label'] == 'person' and
-                            item['confidence'] >= 0.2)]
+
+        filtered_results = [item for item in results if (item['label'] == 'person')]
 
         for item_found in filtered_results:
             cv2.rectangle(images[i], (item_found['topleft']['x'], item_found['topleft']['y']),
                           (item_found['bottomright']['x'], item_found['bottomright']['y']),
                           (0,255,0), 4)
+
     elapsed_time = time.time() - start_time
     print(f'Finished in {elapsed_time}s')
 
